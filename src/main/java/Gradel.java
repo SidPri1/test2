6d9c291e1429b2b8874eaa19dff73d4470d4a328
Siddu <siddupatil119@gmail.com>
2020-02-12 14:55:42 +0530

    adding gardel-java-project-test2 sample project

---
+++ src/main/java/Gradel.java
@@ -0,0 +1,16 @@
+
+public class Gradel {
+	
+	public String Hello() {
+		return "Hello Gradel";
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gradel gradel =new Gradel();
+		System.out.println(gradel.Hello());
+
+	}
+
+}
